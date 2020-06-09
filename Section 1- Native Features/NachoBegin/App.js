8c81dc1be79925280c0751180f617b35c52ca5ef
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-09 20:52:50 +1200

    AppNavigator - Tab

--- Section 1- Native Features/NachoBegin/App.js
+++ Section 1- Native Features/NachoBegin/App.js
@@ -1,23 +1,15 @@
 import React from "react";
-import { Text, Button } from "react-native";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
-import { NavigationContainer, useNavigation } from "@react-navigation/native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import { NavigationContainer } from "@react-navigation/native";
 
-import Screen from "./app/components/Screen";
-// import ImageInputList from "./app/components/ImageInputList";
-// import ListingEditScreen from "./app/screens/ListingEditScreen";
-import LoginScreen from "./app/screens/LoginScreen";
-import RegisterScreen from "./app/screens/RegisterScreen";
-import WelcomeScreen from "./app/screens/WelcomeScreen";
-import AuthNavigator from "./app/navigation/AuthNavigator";
 import navigationTheme from "./app/navigation/navigationTheme";
+import AppNavigator from "./app/navigation/AppNavigator";
+import AuthNavigator from "./app/navigation/AuthNavigator";
 
 export default function App() {
   return (
     <NavigationContainer theme={navigationTheme}>
-      <AuthNavigator />
+      {/* <AuthNavigator /> */}
+      <AppNavigator />
     </NavigationContainer>
   );
 }
