d8410f73a5720e3474bb984405e927f97a777315
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-09 15:26:18 +1200

    ImageInput

--- Section 1- Native Features/NachoBegin/App.js
+++ Section 1- Native Features/NachoBegin/App.js
@@ -1,6 +1,26 @@
-import React from "react";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+import React, { useEffect, useState } from "react";
+
+import Screen from "./app/components/Screen";
+import ImageInput from "./app/components/ImageInput";
 
 export default function App() {
-  return <ListingEditScreen />;
+  const [imageUri, setImageUri] = useState();
+
+  const requestPermission = async () => {
+    const { granted } = await ImagePicker.requestCameraPermissionsAsync();
+    if (!granted) alert("You need to enable permissioin to access your camera");
+  };
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  return (
+    <Screen>
+      <ImageInput
+        imageUri={imageUri}
+        onChangeImage={(uri) => setImageUri(uri)}
+      />
+    </Screen>
+  );
 }
