ee416ed2ebfd1e736256262306e0eb979196cf30
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-09 21:25:56 +1200

    AccountNavigator

--- Section 1- Native Features/NachoBegin/app/navigation/AppNavigator.js
+++ Section 1- Native Features/NachoBegin/app/navigation/AppNavigator.js
@@ -1,9 +1,9 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 
-import AccountScreen from "../screens/AccountScreen";
 import ListingEditScreen from "../screens/ListingEditScreen";
 import FeedNavigator from "./FeedNavigator";
+import AccountNavigator from "./AccountNavigator";
 
 const Tab = createBottomTabNavigator();
 
@@ -11,7 +11,7 @@ const AppNavigator = () => (
   <Tab.Navigator>
     <Tab.Screen name="Feed" component={FeedNavigator} />
     <Tab.Screen name="ListingEdit" component={ListingEditScreen} />
-    <Tab.Screen name="Account" component={AccountScreen} />
+    <Tab.Screen name="Account" component={AccountNavigator} />
   </Tab.Navigator>
 );
 export default AppNavigator;
