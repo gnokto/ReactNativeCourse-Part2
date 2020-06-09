af672533d737fd910f8822cd9509b79c95a59e85
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-10 00:57:06 +1200

    ActivityIndicator AKA loading animation

--- Section 1- Native Features/NachoBegin/app/screens/ListingsScreen.js
+++ Section 1- Native Features/NachoBegin/app/screens/ListingsScreen.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import { FlatList, StyleSheet, ActivityIndicator } from "react-native";
+import { FlatList, StyleSheet } from "react-native";
 
 import Button from "../components/Button";
 import Card from "../components/Card";
@@ -8,6 +8,7 @@ import Screen from "../components/Screen";
 import routes from "../navigation/routes";
 import listingsApi from "../api/listings";
 import AppText from "../components/Text";
+import ActivityIndicator from "../components/ActivityIndicator";
 
 function ListingsScreen({ navigation }) {
   const [listings, setListings] = useState([]);
@@ -39,7 +40,7 @@ function ListingsScreen({ navigation }) {
           <Button title="Retry" onPress={loadListings} />
         </>
       )}
-      <ActivityIndicator animating={true} size={30} />
+      <ActivityIndicator visible={loading} />
       <FlatList
         data={listings}
         keyExtractor={(listing) => listing.id.toString()}
