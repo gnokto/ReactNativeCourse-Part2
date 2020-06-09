edc8f86b6aa34b38710f93e193097ffccdc53e13
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-09 23:52:17 +1200

    Fetching data from API

--- Section 1- Native Features/NachoBegin/app/api/client.js
+++ Section 1- Native Features/NachoBegin/app/api/client.js
@@ -3,4 +3,4 @@ import { create } from "apisauce";
 const apiClient = create({
   baseURL: "http://192.168.1.6:9000/api",
 });
-export default api;
+export default apiClient;
--- Section 1- Native Features/NachoBegin/app/screens/ListingDetailsScreen.js
+++ Section 1- Native Features/NachoBegin/app/screens/ListingDetailsScreen.js
@@ -10,7 +10,7 @@ function ListingDetailsScreen({ route }) {
   console.log(listing);
   return (
     <View>
-      <Image style={styles.image} source={listing.image} />
+      <Image style={styles.image} source={{ uri: listing.images[0].url }} />
       <View style={styles.detailsContainer}>
         <Text style={styles.title}>{listing.title}</Text>
         <Text style={styles.price}>{listing.price}</Text>
