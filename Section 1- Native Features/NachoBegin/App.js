fb6d338ac1aa46b5b83e6e519dd71e7774a51fef
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-09 18:09:10 +1200

    Navigation Examples in App.js

--- Section 1- Native Features/NachoBegin/App.js
+++ Section 1- Native Features/NachoBegin/App.js
@@ -1,9 +1,43 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
+import { Text, Button } from "react-native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { NavigationContainer, useNavigation } from "@react-navigation/native";
 
 import Screen from "./app/components/Screen";
-import ImageInputList from "./app/components/ImageInputList";
-import ListingEditScreen from "./app/screens/ListingEditScreen";
+// import ImageInputList from "./app/components/ImageInputList";
+// import ListingEditScreen from "./app/screens/ListingEditScreen";
+
+const Link = () => {
+  const navigation = useNavigation();
+  return (
+    <Button title="Click" onPress={() => navigation.navigate("TweetDetails")} />
+  );
+};
+
+const Tweets = () => (
+  <Screen>
+    <Text>Tweets</Text>
+    <Link />
+  </Screen>
+);
+
+const TweetDetails = () => (
+  <Screen>
+    <Text>TweetDetails</Text>
+  </Screen>
+);
+const Stack = createStackNavigator();
+const StackNavigator = () => (
+  <Stack.Navigator>
+    <Stack.Screen name="Tweets" component={Tweets} />
+    <Stack.Screen name="TweetDetails" component={TweetDetails} />
+  </Stack.Navigator>
+);
 
 export default function App() {
-  return <ListingEditScreen />;
+  return (
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
+  );
 }
