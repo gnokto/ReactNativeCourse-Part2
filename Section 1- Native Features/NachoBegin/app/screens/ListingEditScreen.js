66cdb25178f9337ea3fd92d15f6aac22131d1bd4
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-10 15:00:18 +1200

    Add beautiful progress bar

--- Section 1- Native Features/NachoBegin/app/screens/ListingEditScreen.js
+++ Section 1- Native Features/NachoBegin/app/screens/ListingEditScreen.js
@@ -87,6 +87,7 @@ function ListingEditScreen() {
   const [progress, setProgress] = useState(0);
 
   handleSubmit = async (listing) => {
+    setProgress(0);
     setUploadVisible(true);
     const result = await listingsApi.addListing(
       { ...listing, location },
