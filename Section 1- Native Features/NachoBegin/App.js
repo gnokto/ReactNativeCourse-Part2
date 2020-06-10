7cdbf0c099b42a7037a0b0fa3652a66f29f82b3e
Ignacio Donovan <ignacio.donovan@xero.com>
2020-06-10 21:34:44 +1200

    OfflineNotice component

--- Section 1- Native Features/NachoBegin/App.js
+++ Section 1- Native Features/NachoBegin/App.js
@@ -4,12 +4,16 @@ import { NavigationContainer } from "@react-navigation/native";
 import navigationTheme from "./app/navigation/navigationTheme";
 import AppNavigator from "./app/navigation/AppNavigator";
 import AuthNavigator from "./app/navigation/AuthNavigator";
+import OfflineNotice from "./app/components/OfflineNotice";
 
 export default function App() {
   return (
-    <NavigationContainer theme={navigationTheme}>
-      {/* <AuthNavigator /> */}
-      <AppNavigator />
-    </NavigationContainer>
+    <>
+      <OfflineNotice />
+      <NavigationContainer theme={navigationTheme}>
+        {/* <AuthNavigator /> */}
+        <AppNavigator />
+      </NavigationContainer>
+    </>
   );
 }
